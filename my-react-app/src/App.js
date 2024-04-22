69c9db058177f317933d6af773b33f91af2ffd26
MahinSulaiman <mahinsulaiman2002@gmail.com>
2024-04-22 18:31:41 +0530

    created sidebar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,34 @@
 
 import './App.css';
+import SideBar from './components/sidebar';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
+    <>
     <div className="App">
 
-      <h1>Hello World!</h1>
+      {/* <h1 className='text-red-500'>Hello World!</h1> */}
+
+      
+     
+
+      
+     
+      
       
     </div>
+    <SideBar/>
+
+    </>
+
+   
+    
   );
 }
 
